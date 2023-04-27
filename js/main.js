f181894bf05ae4b8855f9ea7f05527e2f9bf52eb
nokaml <bimuvek2014@yandex.ru>
2023-04-27 16:57:46 +0300

    add: able to check inputs

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 let table = document.querySelector('.table');
 let button = document.querySelector('.btn1');
+let fullTable = document.querySelector('.fullTable');
 let array = [];
-// +(select1.options[select1.selectedIndex].value);
+
+let selectValues = new URLSearchParams(window.location.search);
+let width = selectValues.get('choice');
+let width1 = selectValues.get('choice1');
+
 
 function tableCreator(num1, num2) {
     for (i = 0; i < num2; i++) {
@@ -17,16 +22,40 @@ function tableCreator(num1, num2) {
     }
 }
 
-let selectValues = new URLSearchParams(window.location.search);
-let width = selectValues.get('choice');
-let width1 = selectValues.get('choice1');
-
-tableCreator(width, width1);
-
-button.addEventListener('click', () => {
+function doArray(table, array) {
     for (let i = 0; i < table.childNodes.length; i++) {
         if (table.childNodes[i].value != undefined) {
             array.push(table.childNodes[i].value);
         }
+    }   
+}
+
+function tableValues(array, num1, num2) {
+    num1 += 2;
+    num2 += 2;
+    for (i = 0; i < num2; i++) {
+        tr = document.createElement('tr');
+        for (j = 0; j < num1; j++) {
+            td = document.createElement('td');
+            td.innerHTML = 1;
+            tr.appendChild(td);
+        }
+        fullTable.appendChild(tr);
     }
-})
+}
+
+tableCreator(width, width1);
+
+button.addEventListener('click', e => {
+    doArray(table, array);
+    for (let i = 0; i < array.length; i++) {
+        if (isNaN(array[i])) {
+            alert('ВВЕДИТЕ ЧИСЛО!');
+            e.preventDefault();
+            break
+        }
+    }
+    location.reload();
+});
+
+tableValues(array, width, width1);
