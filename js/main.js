f2d92f0af749d17442a13bef5ef87751e5801cd9
nokaml <bimuvek2014@yandex.ru>
2023-04-30 18:20:30 +0300

    upd: ability to change .fullTable

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-let table = document.querySelector(".table");
-let button = document.querySelector(".btn1");
-let fullTable = document.querySelector("table");
-let array = [];
+var table = document.querySelector(".table");
+var button = document.querySelector(".btn1");
+var button1 = document.querySelector('.button');
+var fullTable = document.querySelector("table");
 
-let selectValues = new URLSearchParams(window.location.search);
-let width = selectValues.get("choice");
-let width1 = selectValues.get("choice1");
+var array = [];
+
+var selectValues = new URLSearchParams(window.location.search);
+var width = +selectValues.get("choice");
+var width1 = +selectValues.get("choice1");
+
 
 function tableCreator(num1, num2) {
   for (i = 0; i < num2; i++) {
@@ -26,7 +29,7 @@ function doArray(table) {
     if (table.childNodes[i].value != undefined) {
       array.push(table.childNodes[i].value);
     }
-}
+  }
 }
 
 function setCookie(name, value, options = {}) {
@@ -54,39 +57,54 @@ function setCookie(name, value, options = {}) {
   document.cookie = updatedCookie;
 }
 
-function tableValues(num1, num2) {
-    num1 += 2;
-    num2 += 2;
-    for (i = 0; i < num2; i++) {
-        tr = document.createElement('tr');
-        for (j = 0; j < num1; j++) {
-            td = document.createElement('td');
-            td.innerHTML = 1;
-            tr.appendChild(td);
-        }
-        fullTable.appendChild(tr);
+function tableValues(num1, num2, table) {
+  for (i = 0; i < num2; i++) {
+    tr = document.createElement('tr');
+    tr = table.insertRow(i);
+    for (j = 0; j < num1; j++) {
+      tr.insertCell(j).innerHTML = 1;
     }
+  }
 }
 
 tableCreator(width, width1);
 
-button.addEventListener("click", (e) => {
-  doArray(table);
+if (button) {
+  button.addEventListener("click", (e) => {
+    doArray(table);
 
-  for (let i = 0; i < array.length; i++) {
-    setCookie(i, array[i], {secure: true, 'max-age': 5});
-  }
-
-  for (let i = 0; i < array.length; i++) {
-    if (isNaN(array[i])) {
-      alert("ВВЕДИТЕ ЧИСЛО!");
-      e.preventDefault();
-      break;
+    for (let i = 0; i < array.length; i++) {
+      setCookie(i, array[i], { secure: true, "max-age": 5 });
     }
-  }
 
-  location.reload();
-  alert(document.cookie);
-});
+    for (let i = 0; i < array.length; i++) {
+      if (isNaN(array[i])) {
+        alert("ВВЕДИТЕ ЧИСЛО!");
+        e.preventDefault();
+        break;
+      }
+    }
+
+    location.reload();
+    alert(document.cookie);
+  });
+}
+
+if (button1) {
+  button1.addEventListener('click', () => {
+    var select1 = document.querySelector('.select1');
+    var select2 = document.querySelector('.select2');
+    setCookie('select1', select1.options[select1.selectedIndex].value, { secure: true, "max-age": 10 });
+    setCookie('select2', select2.options[select2.selectedIndex].value, { secure: true, "max-age": 10 });
+  })
+}
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
 
 
+tableValues(getCookie('select1'), getCookie('select1'), fullTable);
