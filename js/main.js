6df4311499fd61453a2937de1e729f1e65cca787
nokaml <bimuvek2014@yandex.ru>
2023-05-03 16:31:09 +0300

    upd: another table -_-

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
-var table = document.querySelector(".table");
-var button = document.querySelector(".btn1");
+var table = document.querySelector('.table');
+var button = document.querySelector('.btn1');
 var button1 = document.querySelector('.button');
-var fullTable = document.querySelector("table");
+var fullTable = document.querySelector('.fullTable');
+var row = document.querySelector('.row');
+var column = document.querySelector('.column');
+var table1 = document.querySelector('.table1');
 
 var array = [];
 
@@ -57,16 +60,6 @@ function setCookie(name, value, options = {}) {
   document.cookie = updatedCookie;
 }
 
-function tableValues(num1, num2, table) {
-  for (i = 0; i < num2; i++) {
-    tr = document.createElement('tr');
-    tr = table.insertRow(i);
-    for (j = 0; j < num1; j++) {
-      tr.insertCell(j).innerHTML = 1;
-    }
-  }
-}
-
 tableCreator(width, width1);
 
 if (button) {
@@ -86,7 +79,6 @@ if (button) {
     }
 
     location.reload();
-    alert(document.cookie);
   });
 }
 
@@ -94,8 +86,8 @@ if (button1) {
   button1.addEventListener('click', () => {
     var select1 = document.querySelector('.select1');
     var select2 = document.querySelector('.select2');
-    setCookie('select1', select1.options[select1.selectedIndex].value, { secure: true, "max-age": 10 });
-    setCookie('select2', select2.options[select2.selectedIndex].value, { secure: true, "max-age": 10 });
+    setCookie('select1', select1.options[select1.selectedIndex].value, { secure: true, "max-age": 20 });
+    setCookie('select2', select2.options[select2.selectedIndex].value, { secure: true, "max-age": 20 });
   })
 }
 
@@ -107,4 +99,115 @@ function getCookie(name) {
 }
 
 
-tableValues(getCookie('select1'), getCookie('select1'), fullTable);
+// tableValues(getCookie('select1'), getCookie('select2'), row, column, table1);
+
+/* Defining the tableCreate function */
+function tableCr(rows, cols, data, thead, tfoot) {
+  // 1) Create table and body elements
+  let table = document.createElement('table')
+  let tableBody = document.createElement('tbody')
+
+  // 2) Optional header
+  let headContent = document.createElement('thead')
+  let tr = document.createElement('tr')
+
+  // 2.1) Sets default behavior: Single cell header
+  if (thead && Array.isArray(thead) == false) {
+    let td = document.createElement('td')
+    td.innerHTML = thead // Set header text to argument input
+    td.setAttribute('colspan', cols) // Span header for as many cols as table
+    tr.append(td)
+
+    headContent.append(tr) // append head row to thead element
+    thead = headContent // Make this final value of thead
+  }
+  // 2.2) If "split" is third argument: Creates a multi-cell header
+  if (Array.isArray(thead)) {
+    let i
+    for (i = 0; i < cols; i++) {
+      let td = document.createElement('td')
+      td.id = 'thead' + i
+      td.innerHTML = thead[i]
+      tr.append(td) // append multiple td to head row
+    }
+    headContent.append(tr) // append head row to thead element
+    thead = headContent // Make this final value of thead
+  }
+
+  // 3) Optional footer (text is user input string)
+  if (tfoot) {
+    footElement = document.createElement('tfoot')
+    tr = document.createElement('tr')
+    td = document.createElement('td')
+    td.innerHTML = tfoot // Set text to fourth argument input
+    td.setAttribute('colspan', cols)
+    tr.append(td) // Append single cell to row
+    footElement.append(tr) // Append row to tfoot element
+    tfoot = footElement // Make this final value of tfoot
+  }
+
+  // 4) Create table body rows and cell with loops
+  let i
+  for (i = 0; i < rows; i++) {
+    // Loop to create row
+    let tr = document.createElement('tr')
+
+    let id = i * cols // Nested loop to append cells to rows (first loop id = 0*5; second loop id = 1*5, etc)
+    for (j = 0; j < cols; j++) {
+      let td = document.createElement('td')
+      id++ // increase id by 1 (first loop is 0+1 = 1)
+      if (id == i * cols + 1) {
+        td.classList.add('left-col')
+      }
+      td.innerHTML = id // print id in col cell
+      td.setAttribute('id', 'cell' + id) // set id of element to id
+      tr.append(td) // append col cell to table row
+      // Repeats until j < column numbers entered by user
+
+      if (data) {
+        td.innerHTML = data[id - 1]
+      }
+    }
+
+    tableBody.append(tr)
+  }
+
+  // 5) Append head, body and footer
+  if (thead) {
+    table.append(thead)
+  }
+  table.append(tableBody)
+  if (tfoot) {
+    table.append(tfoot)
+  }
+
+  // Show table in console
+  fullTable.append(table);
+
+  // 6) Return a value for the function
+  return table
+}
+
+function A(num1, num2) {
+  
+  for (i = 0; i < num2; i++) {
+    for (j = 0; j < num1; j++) {
+      return 'A' + (i + 1);
+    }
+  }
+}
+
+// Define data input
+const data = ["Monday", "Cereal", "Steamed rice", "Baked Potatoes",
+              "Tuesday", "Bagel", "Spaghetti bolognese", "Pasta salad",
+              "Wednesday", "Oatmeal", "Pasta", "Burger",
+              "Thursday", "Cereal", "Fish and chips", "Fried Rice",
+              "Friday", "Fried breakfast", "Sandwiches", "Lasange"
+            ]
+
+// Define headings
+const headings = ['Игроки', A(getCookie('select1'), getCookie('select2')), 'A = min(ai)']
+
+/* Call tableCreate */
+alert(document.cookie);
+tableCr(5,4,data,headings,'Created by JavaScript');
